ec93c3ffbb72f8b0790a577fed3f5e9d4f12ecaf
Marixavq <rm566357@fiap.com.br>
2026-05-23 16:21:05 -0300

    Paginação e ordenação

--- src/main/java/com/fiap/challengepetcenter/repository/PetRepository.java
+++ src/main/java/com/fiap/challengepetcenter/repository/PetRepository.java
@@ -1,12 +1,9 @@
 package com.fiap.challengepetcenter.repository;
 
 import com.fiap.challengepetcenter.model.Pet;
-import com.fiap.challengepetcenter.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
     Page<Pet> findByNomeContaining(String nome, Pageable pageable);

9bba2f48cde1e018ba3212d0126b3172fab7cd4f
Marixavq <rm566357@fiap.com.br>
2026-08-23 18:47:40 -0300

    Consertando Swagger

--- src/main/java/com/fiap/challengepetcenter/config/SecurityConfig.java
+++ src/main/java/com/fiap/challengepetcenter/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
+                                "/api-docs/**",
                                 "/h2-console/**"
                         ).permitAll()
 

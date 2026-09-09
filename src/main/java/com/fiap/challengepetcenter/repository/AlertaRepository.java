60655e500e21fac935dfd4e1835d1e10893ee09c
Marixavq <rm566357@fiap.com.br>
2026-09-08 23:38:00 -0300

    AlertaService

---
+++ src/main/java/com/fiap/challengepetcenter/repository/AlertaRepository.java
@@ -0,0 +1,9 @@
+package com.fiap.challengepetcenter.repository;
+
+import com.fiap.challengepetcenter.model.Alerta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlertaRepository extends JpaRepository<Alerta, Long> {
+
+
+}
--- src/main/java/com/fiap/challengepetcenter/service/AlertaService.java
+++ src/main/java/com/fiap/challengepetcenter/service/AlertaService.java
@@ -1,9 +1,82 @@
 package com.fiap.challengepetcenter.service;
 
+import com.fiap.challengepetcenter.dto.request.AlertaRequestDTO;
+import com.fiap.challengepetcenter.dto.response.AlertaResponseDTO;
+import com.fiap.challengepetcenter.model.Alerta;
+import com.fiap.challengepetcenter.model.Pet;
+import com.fiap.challengepetcenter.model.User;
+import com.fiap.challengepetcenter.model.Veterinario;
+import com.fiap.challengepetcenter.repository.AlertaRepository;
+import com.fiap.challengepetcenter.repository.PetRepository;
+import com.fiap.challengepetcenter.repository.UserRepository;
+import com.fiap.challengepetcenter.repository.VeterinarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AlertaService {
 
+    private final PetRepository petRepository;
+    private final AlertaRepository alertaRepository;
+    private final UserRepository userRepository;
+    private final VeterinarioRepository veterinarioRepository;
+
+
+    @Autowired
+    public AlertaService(PetRepository petRepository, AlertaRepository alertaRepository, UserRepository userRepository, VeterinarioRepository veterinarioRepository) {
+        this.petRepository = petRepository;
+        this.alertaRepository = alertaRepository;
+        this.userRepository = userRepository;
+        this.veterinarioRepository = veterinarioRepository;
+
+    }
+
+    @Transactional
+    public AlertaResponseDTO salvar(AlertaRequestDTO requestDTO) {
+
+        User usuarioLogado = getUsuarioAutenticado();
+        Veterinario veterinario = veterinarioRepository
+                .findByUserId(usuarioLogado.getId())
+                .orElseThrow(() -> new RuntimeException("Veterinário não encontrado"));
+
+        Pet pet = petRepository.findById(requestDTO.petId())
+                .orElseThrow(() -> new RuntimeException("Pet não encontrado"));
+
+        Alerta alerta = new Alerta();
+        alerta.setPet(pet);
+        alerta.setVeterinario(veterinario);
+        alerta.setTipo(requestDTO.tipo());
+        alerta.setTitulo(requestDTO.titulo());
+        alerta.setDescricao(requestDTO.descricao());
+        alerta.setDataInicio(requestDTO.dataInicio());
+        alerta.setFrequenciaHoras(requestDTO.frequenciaHoras());
+        alerta.setDataFim(requestDTO.dataFim());
+        alerta.setAtivo(true);
+
+        Alerta alertaSalvo = alertaRepository.save(alerta);
+
+        return AlertaResponseDTO.fromEntity(alertaSalvo);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<AlertaResponseDTO> listarTodos(Pageable pageable) {
+        return alertaRepository.findAll(pageable)
+                .map(AlertaResponseDTO::fromEntity);
+    }
+
+
+    // Usuário autenticado pelo JWT
+    private User getUsuarioAutenticado() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
 
 }
